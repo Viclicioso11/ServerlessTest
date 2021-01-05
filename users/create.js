515c4b39627544e8f2b18d3adbe573ab097aab00
Viclicioso <victorabud11@gmail.com>
2021-01-04 19:59:21 -0600

    probando nodejs con serverless

---
+++ users/create.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const user = JSON.parse(event.body);
+  if (typeof user.name !== 'string' || typeof user.email !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the user.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      name: user.name,
+      email: user.email,
+      createdAt: timestamp,
+    },
+  };
+
+  // write the user to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the user item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
